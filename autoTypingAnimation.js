ebe4f443df092146c344e490e883cfd2261d7b88
Stazzyii <emediongodiong001@gmail.com>
2024-07-21 15:24:54 +0100

    Add files via upload

---
+++ autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function () {
+  $('.about-info-inner').typed({
+    strings: [
+      "<span>Hi, I am Emediong Odiong</span>  As a dedicated computer science student, I am passionate about leveraging technology to solve complex problems. I am currently honing my skills in programming languages such as Python, and Java, while also exploring the fascinating worlds of artificial intelligence and machine learning. My academic journey is complemented by hands-on projects, where I apply theoretical knowledge to build innovative software solutions. I aspire to contribute to cutting-edge developments in technology and am eager to collaborate on exciting ventures that push the boundaries of what is possible in the digital age."
+    ],
+    typeSpeed: 7,
+    contentType: 'html'
+  });
+}, 100);
